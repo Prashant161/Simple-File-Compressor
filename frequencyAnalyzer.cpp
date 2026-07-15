0ec841990e91444d01c5b899db8c09e5670b40e3
Prashant161 <prashantacharya161@gmail.com>
2026-07-16 03:20:07 +0545

    clear

--- frequencyAnalyzer.cpp
+++ frequencyAnalyzer.cpp
@@ -1,12 +1,1 @@
-#include<iostream>
-#include<string>
-using namespace std;
-// int frequency( char* ch1, char* ch2){
-    
-// }
-int main(){
-    string message;
-    cout << "Enter the message: " << endl;
-    getline( cin, message);
-
-}
+//directly try to create frequency table
