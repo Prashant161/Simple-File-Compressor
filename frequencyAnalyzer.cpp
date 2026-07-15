f06bfd53fdf825a3b13303ad400a3492a9540e62
Prashant161 <prashantacharya161@gmail.com>
2026-07-15 21:31:47 +0000

    frequency of letters using basic logic

---
+++ frequencyAnalyzer.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+using namespace std;
+// int frequency( char* ch1, char* ch2){
+    
+// }
+int main(){
+    string message;
+    cout << "Enter the message: " << endl;
+    getline( cin, message);
+
+}
