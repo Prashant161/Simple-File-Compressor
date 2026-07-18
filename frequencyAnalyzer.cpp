411a17d2dda9a1e0e24a5ebae7ebf0738e737b27
Prashant161 <prashantacharya161@gmail.com>
2026-07-18 13:53:11 +0545

    used lambda sorting of vector directly instead of bubble sort

--- frequencyAnalyzer.cpp
+++ frequencyAnalyzer.cpp
@@ -9,31 +9,31 @@ struct Frequency{
     int count;
 };
 int main(){
-    int frequency[256] = {0}, count = 0;
+    int frequency[256] = {0};
     string message;
     vector<Frequency> f;
-    struct Frequency temp;
+
     cout << "Enter the input: " << endl;
     getline(cin, message);
+
     for (char ch: message){
         frequency[ch]++;
     }
+
     for( int i = 0; i < 256; i++){
+
         if(frequency[i]!=0){
             f.push_back({(char)i, frequency[i]});
         }
     }
+
+    sort(f.begin(), f.end(), [](const Frequency& a, const Frequency& b){
+        return a.count>b.count;
+    });
+
     for( int i = 0; i < f.size(); i++){
-        for( int j = i+1; j < f.size(); j++){
-            if (f[i].count<f[j].count){
-                temp = f[i];
-                f[i] = f[j];
-                f[j] = temp;
-            }
-        }
-    }
-    for ( int i = 0; i < f.size(); i++){
         cout << f[i].ch << " : " << f[i].count << endl;
     }
+    
     return 0;
 }
